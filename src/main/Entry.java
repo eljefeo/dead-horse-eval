3c3cc9c079606dbf225854610e54282f7ce8f423
eljefeo <jeffnavabiz@yahoo.com>
2015-05-16 15:24:58 -0700

    some readability

--- src/main/Entry.java
+++ src/main/Entry.java
@@ -4,8 +4,8 @@ public class Entry {
 
 	 public static void main (String[] args) throws java.lang.Exception
 		{  
-		 //EvalTest.showRandomizerDiagnostics(15000000);
-		 EvalTest.randomizerSpeedTest(10000000);
+		// EvalTest.showRandomizerDiagnostics(15000000);
+		 EvalTest.randomizerSpeedTest(15000000);
 		//EvalTest.testEveryHand();
 		}
 }
