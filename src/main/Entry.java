1fdeecfa92d80c801e90a7cc43c6f5620438b829
eljefeo <jeffnavabiz@yahoo.com>
2015-08-13 16:21:56 -0700

    run on intel

--- src/main/Entry.java
+++ src/main/Entry.java
@@ -11,10 +11,10 @@ public class Entry {
 		//EvalTestPlayground.randomizerSpeedTest7Card(10000000); 
 		//EvalTestPlayground.randomizerSpeedTest7Cardr(10000000); 
 		
-		 EvalTestPlayground.randomizerSpeedTest7Card(10040000);
+		 //EvalTestPlayground.randomizerSpeedTest7Card(10000000);
 		
 		 
-		 //EvalTestPlayground.randomizerSpeedTest5Card(20000000); // speed average : 20000000 hands in 0.248193276 seconds = 80Mhps
+		 EvalTestPlayground.randomizerSpeedTest5Card(20000000); // speed average : 20000000 hands in 0.183553937 seconds = 108Mhps
 		// EvalTestPlayground.randomizerSpeedTest5Card(10000000);
 		 //System.out.println(EvalTestPlayground.humanDecodeEval("9d", "9c", "9h", "6s", "9s"));
 		 //EvalTestPlayground.testEveryHand5();
