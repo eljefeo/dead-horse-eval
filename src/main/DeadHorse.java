16fe8ad701fa4bb1a7dca320d60d4e01514f65c4
eljefeo <jeffnavabiz@yahoo.com>
2021-06-03 03:26:18 -0700

    PLEASE DID I BREAK IT??

--- src/main/DeadHorse.java
+++ src/main/DeadHorse.java
@@ -5,7 +5,7 @@ public class DeadHorse {
 	public static int eval5(int a,int b,int c,int d,int e){
 		int x=(a^b^c^d^e)&8191,y=(a|b|c|d|e)&8191,z=y^x,v=y&y-1;
 		if((v&=v-1)==0)
-			return (a+b+c+d+e-&8191)==(8191&(y^x)<<2) 
+			return (a+b+c+d+e-x&8191)==(8191&(y^x)<<2) 
 			  ?0x1C000000|x|z<<13:0x18000000|z|x<<13; //4 of a kind or full house
 		else if((v&=v-1)==0)
 			return z!=0?0x8000000|x|z<<13
@@ -14,6 +14,21 @@ public class DeadHorse {
 		boolean strt=0x1F1D100%y==0,flsh=(a&b&c&d&e)!=0;
 		return strt?(x==4111?15:x)|(flsh?0x20000000:0x10000000):flsh?0x14000000:x;
 	}  
+	
+	/*
+ 	//So ugly its beautiful
+public static int eval5(int a, int b, int c, int d, int e){
+	int x=(a^b^c^d^e)&0x1FFF,y=(a|b|c|d|e)&0x1FFF,z=(y^x),v=y&y-1;
+	boolean strt=0x1F1D100%y==0,flsh=(a&b&c&d&e)!=0;
+	return (v=(v&=v-1)==0?2:(v&=v-1)==0?3:(v&=v-1)==0?4:5)==3?z==0
+		?0xC000000|(v=((a&b)==(a&0x1FFF)?a:(c&d)==(c&0x1FFF)?c:e)&0x1FFF^y)|v<<13
+		:0x8000000|x|z<<13:v==4?0x4000000|x|z<<13
+		:v==2?(a+b+c+d+e-x&0x1FFF)==(0x1FFF&(y^x)<<2)
+		?0x1C000000|x|z<<13:0x18000000|z|x<<13
+		:strt&&flsh?0x20000000|(x==0x100F?15:x)
+		:strt?0x10000000|(x==0x100F?15:x):flsh?0x14000000|x:x;
+}
+ */
 
 	public static String bin32(int i){
 		return String.format("%32s", Integer.toBinaryString(i)).replace(' ', '0');
@@ -539,20 +554,7 @@ public class DeadHorse {
 	
 	
 	
-	/*
-	 	//So ugly its beautiful
-	public static int eval5(int a, int b, int c, int d, int e){
-		int x=(a^b^c^d^e)&0x1FFF,y=(a|b|c|d|e)&0x1FFF,z=(y^x),v=y&y-1;
-		boolean strt=0x1F1D100%y==0,flsh=(a&b&c&d&e)!=0;
-		return (v=(v&=v-1)==0?2:(v&=v-1)==0?3:(v&=v-1)==0?4:5)==3?z==0
-			?0xC000000|(v=((a&b)==(a&0x1FFF)?a:(c&d)==(c&0x1FFF)?c:e)&0x1FFF^y)|v<<13
-			:0x8000000|x|z<<13:v==4?0x4000000|x|z<<13
-			:v==2?(a+b+c+d+e-x&0x1FFF)==(0x1FFF&(y^x)<<2)
-			?0x1C000000|x|z<<13:0x18000000|z|x<<13
-			:strt&&flsh?0x20000000|(x==0x100F?15:x)
-			:strt?0x10000000|(x==0x100F?15:x):flsh?0x14000000|x:x;
-	}
-	 */
+	
 	
 }
 
